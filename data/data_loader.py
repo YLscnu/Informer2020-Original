a5b16e0a3e53bf12cf29ab56f65c7c009167a96f
YL <110099342+YLscnu@users.noreply.github.com>
2023-01-16 22:03:09 +0800

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -61,16 +61,16 @@ class Dataset_ETT_hour(Dataset):
 
         if self.scale:
             train_data = df_data[border1s[0]:border2s[0]]
-            #encoder = OneHotEncoder()
-            #TempA = torch.arange(256)
-            #TempB = TempA.reshape(256,1)
-            #encoder.fit(TempB)
-            #train_data = encoder.transform(train_data).toarray()
-            self.scaler.fit(train_data.values)
-            data = self.scaler.transform(df_data.values)
+            encoder = OneHotEncoder()
+            TempA = torch.arange(256)
+            TempB = TempA.reshape(256,1)
+            encoder.fit(TempB)
+            train_data = encoder.transform(train_data).toarray()
+#             self.scaler.fit(train_data.values)
+#             data = self.scaler.transform(df_data.values)
         else:
-            data = df_data.values
-            #data = encoder.transform(df_data).toarray()
+            #data = df_data.values
+            data = encoder.transform(df_data).toarray()
             
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
